2ff42a6b10f9cc95e5d10d928d6a99e3ec4b4f22
Chandrika <sejutispr.dey01@gmail.com>
2020-10-07 14:51:59 -0400

    numb

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Integer x = 15; // boxes int to an Integer object
+        x =  x + 25;   // unboxes thed Integer to an int
+        System.out.println(x);
+    }
+}
